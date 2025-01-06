ad475b7ac1d6fde81ea980d36e729332739753be
valadao-davi <davi.raichu@gmail.com>
2025-01-06 16:32:29 -0300

    adicionado renderizacao de listas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Task from "./components/Task";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      description: "Estudar React e Java",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Ingles",
+      description: "Estudar 2 horas por dia de inglês",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Arrumar a casa",
+      description: "Lavar a louça, arrumar o quarto, varrer o chão",
+      isCompleted: false,
+    },
+  ]);
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -9,7 +31,7 @@ function App() {
           Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Task />
+        <Task tasks={tasks}/>
       </div>
     </div>
   );
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,13 @@
-function Task() {
-   <h1>Tasks</h1>;
+function Task(props) {
+  return (
+    <ul className="space-y-4">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md ">
+          {task.title}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Task;
