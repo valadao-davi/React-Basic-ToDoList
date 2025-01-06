512d0d3f111b7b0925948934d4f047e9511a56e4
valadao-davi <davi.raichu@gmail.com>
2025-01-06 16:20:44 -0300

    estilizado main

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import AddTask from "./components/AddTask";
 import Task from "./components/Task";
 
 function App() {
-
   return (
-    <div>
-      <h1 className="text-red-500 text-3xl">Gerenciador de Tarefas</h1>
-      < AddTask />
-      <Task />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Task />
+      </div>
     </div>
   );
 }
