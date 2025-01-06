c85821f4e50067e364f9d182a3696cca458ab4d5
valadao-davi <davi.raichu@gmail.com>
2025-01-06 19:53:18 -0300

    concerto no useState do app

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import Title from './components/Title';
 import { v4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] =
-    useState(JSON.parse(localStorage.getItem("tasks"))) || [];
-
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+  
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
